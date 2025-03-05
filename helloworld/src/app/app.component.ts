0045c3eefa8e8c6c1ef267298e931cee515a80b6
pankaj143p <pankaj114477pankaj@gmail.com>
2025-03-05 11:38:58 +0530

    [Pankaj] : updated

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-  <h1>Hello World</h1>
-  `
-  // templateUrl: './app.component.html',
-  // styleUrl: './app.component.scss'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'helloworld';
+  ngOnInit() {
+    this.title = 'Hello World from BridgeLabz.';
+  }
 }
