7cc19934c5ba045f28591f4a4f5fd23685b1e80b
pankaj143p <pankaj114477pankaj@gmail.com>
2025-03-05 12:43:18 +0530

    [Pankaj] : Ability To launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -10,7 +10,12 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'helloworld';
   imgUrl='logo.png';
+  url='https://www.bridgelabz.com';
   ngOnInit() {
     this.title = 'Hello World from BridgeLabz.';
+  }
+  onClick($event: MouseEvent): void {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, '_blank'); 
   }
 }
