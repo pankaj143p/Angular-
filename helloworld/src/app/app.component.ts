a6f5d45ebd3f48da2909f712feb6d8d8a6179193
pankaj143p <pankaj114477pankaj@gmail.com>
2025-03-05 15:04:50 +0530

    [Pankaj] : bug fixes

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -11,12 +11,21 @@ export class AppComponent {
   imgUrl='logo.png';
   url='https://www.bridgelabz.com';
   name : string ="";
+  nameError : string ="";
   ngOnInit() {
     this.title = 'Hello World from BridgeLabz.';
   }
   onClick($event: MouseEvent): void {
     console.log("Save button is clicked!", $event);
     window.open(this.url, '_blank'); 
+  }
+  onInput(){
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.name)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
 
